e4f046ce96f46ddc8add3947994ea01809ea2ed3
Tsuyoshi Yamamoto <tsuyoshi.yamamoto@jrits.ricoh.co.jp>
2016-02-01 12:54:43 +0900

    Tutorialの1st chapterまで完了

---
+++ HelloMetal/StaticVarReference.swift
@@ -0,0 +1,11 @@
+//
+//  StaticVarReference.swift
+//  HelloMetal
+//
+//  Created by tyamamo on 2016/01/28.
+//  Copyright © 2016年 local. All rights reserved.
+//
+
+import Foundation
+
+let testString: String = "test"
--- HelloMetal/ViewController.swift
+++ HelloMetal/ViewController.swift
@@ -1,18 +1,48 @@
-//
-//  ViewController.swift
-//  HelloMetal
-//
-//  Created by tyamamo on 2016/01/27.
-//  Copyright © 2016年 local. All rights reserved.
-//
-
 import UIKit
+import MetalKit
+import QuartzCore
 
 class ViewController: UIViewController {
-
+    var device: MTLDevice! = nil
+    var metalLayer: CAMetalLayer! = nil
+    var vertexBuffer: MTLBuffer! = nil
+    var pipelineState: MTLRenderPipelineState! = nil
+    var commandQueue: MTLCommandQueue! = nil
+    var timer: CADisplayLink! = nil
+    
+    let vertexData:[Float] = [
+        0.0, 1.0, 0.0,
+        -1.0, -1.0, 0.0,
+        1.0, -1.0, 0.0]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        device = MTLCreateSystemDefaultDevice()
+        metalLayer = CAMetalLayer()
+        metalLayer.device = device
+        metalLayer.pixelFormat = .BGRA8Unorm
+        metalLayer.framebufferOnly = true
+        metalLayer.frame = view.layer.frame
+        view.layer.addSublayer(metalLayer)
+        
+        let dataSize = vertexData.count * sizeofValue(vertexData[0])
+        vertexBuffer = device.newBufferWithBytes(vertexData, length: dataSize, options: MTLResourceOptions.CPUCacheModeDefaultCache)
+        
+        let defaultLibrary = device.newDefaultLibrary()
+        let fragmentProgram = defaultLibrary!.newFunctionWithName("basic_fragment")
+        let vertexProgram = defaultLibrary!.newFunctionWithName("basic_vertex")
+        
+        let pipelineStateDescriptor = MTLRenderPipelineDescriptor()
+        pipelineStateDescriptor.vertexFunction = vertexProgram
+        pipelineStateDescriptor.fragmentFunction = fragmentProgram
+        pipelineStateDescriptor.colorAttachments[0].pixelFormat = .BGRA8Unorm
+        
+        try! self.pipelineState = self.device.newRenderPipelineStateWithDescriptor(pipelineStateDescriptor)
+        
+        commandQueue = device.newCommandQueue()
+
+        timer = CADisplayLink(target: self, selector: Selector("gameloop"))
+        timer.addToRunLoop(NSRunLoop.mainRunLoop(), forMode: NSDefaultRunLoopMode)        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +51,31 @@ class ViewController: UIViewController {
     }
 
 
+    func render() {
+        let drawable: CAMetalDrawable? = metalLayer.nextDrawable()
+        
+        let renderPassDescriptor = MTLRenderPassDescriptor()
+        renderPassDescriptor.colorAttachments[0].texture = drawable!.texture
+        renderPassDescriptor.colorAttachments[0].loadAction = .Clear
+        renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColorMake(0.0, 104.0/255.0, 5.0/255.0, 1.0)
+        
+        let commandBuffer = commandQueue.commandBuffer()
+        let renderEncoderOpt = commandBuffer.renderCommandEncoderWithDescriptor(renderPassDescriptor)
+        if let renderEncoder: MTLRenderCommandEncoder = renderEncoderOpt {
+            renderEncoder.setRenderPipelineState(pipelineState)
+            renderEncoder.setVertexBuffer(vertexBuffer, offset: 0, atIndex:0)
+            renderEncoder.drawPrimitives(.Triangle, vertexStart: 0, vertexCount: 3, instanceCount: 1)
+            renderEncoder.endEncoding()
+        }
+        
+        commandBuffer.presentDrawable(drawable!)
+        commandBuffer.commit()
+    }
+    
+    func gameloop() {
+        autoreleasepool {
+            self.render()
+        }
+    }
 }
 
